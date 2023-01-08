8c3d4270c3b04e35e558cfca3d758510c25c2dc8
TheresaRoss <thutgreybear@gmail.com>
2023-01-08 20:50:11 +0700

    set font head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+
 import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     
+        <h1 className="text-3xl hfont ">
+      Hello world!
+    </h1>
+    <p>dddddddddddddddddddddd</p>
     </>
   )
 }
