bf8264483b6080979c07547825bbf62256e8c6e5
TheresaRoss <thutgreybear@gmail.com>
2023-01-10 22:12:48 +0700

    fix interface layout

--- pages/courses/instructor_view/[id].tsx
+++ pages/courses/instructor_view/[id].tsx
@@ -3,98 +3,173 @@ import Router from "next/router";
 import { useEffect, useState } from "react";
 import { Headerr } from "../../../component/headerr";
 import prisma from "../../../lib/prisma";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
 
-const CourseMenu: React.FC = ({ course: { id: courseId, title, description, instructor_name }, registered_students }) => {
-    const [loading, setLoading] = useState(true)
+const CourseMenu: React.FC = ({
+  course: { id: courseId, title, description, instructor_name },
+  registered_students,
+}) => {
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        //for new features
-    }, []);
+  useEffect(() => {
+    //for new features
+  }, []);
 
-    const deleteHandler = async () => {
-        try {
-            const response = await fetch(`/api/course/${courseId}`, {
-                method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
-            });
-            const json = await response.json();
-            console.log(json);
-            Router.push('/instructor/main');
-        } catch (error) {
-        } finally {
-
-        }
+  const deleteHandler = async () => {
+    try {
+      const response = await fetch(`/api/course/${courseId}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      const json = await response.json();
+      console.log(json);
+      Router.push("/instructor/main");
+    } catch (error) {
+    } finally {
     }
+  };
+  const RegisStudent = () => {
+    return (
+      <div className="overflow-auto bg-purple-200 max-h-36  p-5">
+        <h2>Registered Students</h2>
+        {registered_students.length ? (
+          registered_students.map((student) => (
+            <div key={student.id}>
+              <p>Name: {student.name}</p>
+              <p className="text-xs">Email: {student.email}</p>
+            </div>
+          ))
+        ) : (
+          <h2>no registered students</h2>
+        )}
+      </div>
+    );
+  };
 
-    const Mainbody = () => {
-        return (
-            <>
-                <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={() => deleteHandler()}>Delete</button>
+  const Announcements = () => {
+    return (
+      <div className="bg-red-200 py-5 px-5">
+        <FontAwesomeIcon icon={faBullhorn} />
+        <h2>Announcements</h2>
+      </div>
+    );
+  };
 
-                <h1>{title}</h1>
-                <small>Description: {description}</small>
+  const ScoreManage = () => {
+    return (
+      <div className="bg-amber-200 py-5 px-5">
+        {" "}
+        <h2>Score Manage</h2>
+      </div>
+    );
+  };
 
-                <div>
-                    <p>Registered Students</p>
-                    {
-                        registered_students.length ?
-                            (registered_students.map(student => (
-                                <div key={student.id}>
-                                    <h2>Name: {student.name}</h2>
-                                    <small>Email: {student.email}</small>
-                                </div>
-                            ))) :
-                            <h2>no registered students</h2>
-                    }
-                </div>
+  const DiscussBoard = () => {
+    return (
+      <div className="bg-blue-200 py-5 px-5">
+        <h2>Discussion Board</h2>
+      </div>
+    );
+  };
+  const Assignment = () => {
+    return (
+      <div className="bg-green-200 py-5 px-5">
+        <h2>Assignments</h2>
+      </div>
+    );
+  };
 
-            </>
-        )
+  const CourseMaterials = () => {
+    return (
+      <div className="bg-green-200 py-5 px-5">
+        <h2>Course Materials</h2>
+      </div>
+    );
+  };
 
-    }
+  const Mainbody = () => {
+    return (
+      <>
+        <button
+          className="bg-red-500 text-white py-2 px-4 rounded"
+          onClick={() => deleteHandler()}
+        >
+          Delete
+        </button>
 
-    return (<>
+        <h1>{title}</h1>
+        <p>Description: {description}</p>
+      </>
+    );
+  };
 
-        <Headerr />
-        <div className="pt-10 px-48">
-            <h1 style={{color: "red"}}>FOR DEBUGGING : THIS PAGE IS FOR INSTRUCTOR VIEW OF COURSE MENU</h1>
+  return (
+    <>
+      <Headerr />
+      <div className="pt-10 px-48">
+        <h1 style={{ color: "red" }}>
+          FOR DEBUGGING : THIS PAGE IS FOR INSTRUCTOR VIEW OF COURSE MENU
+        </h1>
+        <div className="grid grid-cols-3 grid-rows-1 pt-10">
+          <div>
             <Mainbody />
+          </div>
+          <div className="col-span-2 grid grid grid-cols-2 gap-5 grid-rows-3">
+            <RegisStudent />
+            <Announcements />
+            <ScoreManage />
+            <DiscussBoard />
+            <Assignment />
+            <CourseMaterials />
+          </div>
         </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-// this function will check for 
+// this function will check for
 // course details, instructor name, lists of registered students
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const courseId = Number(Array.isArray(context.params.id) ? context.params.id[0] : context.params.id)
-    const course = await prisma.course.findUnique({
-        where: { id: courseId },
-        include: { instructor: true },
-    })
+  const courseId = Number(
+    Array.isArray(context.params.id) ? context.params.id[0] : context.params.id
+  );
+  const course = await prisma.course.findUnique({
+    where: { id: courseId },
+    include: { instructor: true },
+  });
 
-    const enrolls = await prisma.enroll.findMany({
-        where: {
-            courseId: courseId,
-        },
-    });
-    const studentIdArray = enrolls.map((e) => e.studentId);
+  const enrolls = await prisma.enroll.findMany({
+    where: {
+      courseId: courseId,
+    },
+  });
+  const studentIdArray = enrolls.map((e) => e.studentId);
 
-    const students = await prisma.student.findMany({
-        where: {
-            id: {
-                in: studentIdArray
-            }
-        },
+  const students = await prisma.student.findMany({
+    where: {
+      id: {
+        in: studentIdArray,
+      },
+    },
 
-        select: {
-            id: true,
-            email: true,
-            name: true,
-        },
-    });
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
 
-    return { props: { course: { ...JSON.parse(JSON.stringify(course)), instructor_name: course.instructor.name }, registered_students: students } }
-}
+  return {
+    props: {
+      course: {
+        ...JSON.parse(JSON.stringify(course)),
+        instructor_name: course.instructor.name,
+      },
+      registered_students: students,
+    },
+  };
+};
 
 export default CourseMenu;
--- pages/courses/student_view/[id].tsx
+++ pages/courses/student_view/[id].tsx
@@ -3,104 +3,152 @@ import Router from "next/router";
 import { useEffect, useState } from "react";
 import { Headerr } from "../../../component/headerr";
 import prisma from "../../../lib/prisma";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
 
-const CourseMenu: React.FC = ({ course: { id: courseId, title, description, instructor_name }}) => {
-    // for some loading
-    const [loading, setLoading] = useState(true)
+const CourseMenu: React.FC = ({
+  course: { id: courseId, title, description, instructor_name },
+}) => {
+  // for some loading
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    //adding font awesome
+    // for some future features
+  }, []);
 
-    useEffect(() => {
-        // for some future features
-    }, []);
-
-
-    const leaveHandler = async () => {
-        const studentId = parseInt(localStorage.getItem('id'));
-        console.log(typeof courseId);
-        console.log(typeof studentId);
-        const body = {
-            studentId: studentId,
-            courseId: courseId
-        };
-        try {
-            const response = await fetch(`/api/enroll`, {
-                method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body)
-            });
-            const json = await response.json();
-            // once leave the course, redirect them t
-            Router.push('/student/dashboard');
-            // console.log(loading);
-        } catch (error) {
-            // setError(error);
-        } finally {
-            // setLoading(false);
-            // console.log(loading);
-        }
+  const leaveHandler = async () => {
+    const studentId = parseInt(localStorage.getItem("id"));
+    console.log(typeof courseId);
+    console.log(typeof studentId);
+    const body = {
+      studentId: studentId,
+      courseId: courseId,
+    };
+    try {
+      const response = await fetch(`/api/enroll`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const json = await response.json();
+      // once leave the course, redirect them t
+      Router.push("/student/dashboard");
+      // console.log(loading);
+    } catch (error) {
+      // setError(error);
+    } finally {
+      // setLoading(false);
+      // console.log(loading);
     }
+  };
 
-    const Mainbody = () => {
-        return (
-            <>
-                <h1>{title}</h1>
-                <small>Description: {description}</small>
-                <p>Instructor: {instructor_name}</p>
+  const Mainbody = () => {
+    return (
+      <>
+        <h1>{title}</h1>
+        <small>Description: {description}</small>
+        <p>Instructor: {instructor_name}</p>
+      </>
+    );
+  };
 
-            </>
-        )
-    }
+  const Announcements = () => {
+    return (
+      <div className="bg-red-200 py-5 px-5">
+        <FontAwesomeIcon icon={faBullhorn} />
+        <h2>Announcements</h2>
+      </div>
+    );
+  };
+  const LiveChat = () => {
+    return (
+      <div className="bg-emerald-200 py-5 px-5">
+        {" "}
+        <h2>Live Chats</h2>
+      </div>
+    );
+  };
+  const ScoreGraphs = () => {
+    return (
+      <div className="bg-amber-200 py-5 px-5">
+        {" "}
+        <h2>Score Graphs</h2>
+      </div>
+    );
+  };
+  const DiscussBoard = () => {
+    return (
+      <div className="bg-blue-200 py-5 px-5">
+        <h2>Discussion Board</h2>
+      </div>
+    );
+  };
+  const CourseMaterials = () => {
+    return (
+      <div className="bg-green-200 py-5 px-5">
+        <h2>Course Materials</h2>
+      </div>
+    );
+  };
+  const Assignment = () => {
+    return (
+      <div className="bg-purple-200 py-5 px-5">
+        <h2>Assignments</h2>
+      </div>
+    );
+  };
 
-    const Announcements = () => {
-        return (
-            <div className="bg-red-400 py-5 px-5">
-                Announcements
-            </div>
-        )
-    }   
-    const LiveChat = () => {
-        return (
-            <div className="bg-emerald-500 py-5 px-5">
-                Live Chat
-            </div>
-        )
-    }   
-    const ScoreGraphs = () => {
-        return (
-            <div className="bg-amber-500 py-5 px-5">
-                Score Graphs
-            </div>
-        )
-    }   
-
-    return (<>
-
-        <Headerr />
-        <div className="pt-10 px-48">
-            <h1 style={{ color: "red" }}>FOR DEBUGGING : THIS PAGE IS FOR STUDENT VIEW OF COURSE MENU</h1>
+  return (
+    <>
+      <Headerr />
+      <div className="pt-10 px-48">
+        <h1 style={{ color: "red" }}>
+          FOR DEBUGGING : THIS PAGE IS FOR STUDENT VIEW OF COURSE MENU
+        </h1>
+        <div className="grid grid-cols-3 grid-rows-1 pt-10">
+          <div>
             <Mainbody />
-            <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={() => leaveHandler()}>Drop</button>
-
+            <button
+              className="bg-red-500 text-white py-2 px-4 rounded"
+              onClick={() => leaveHandler()}
+            >
+              Drop
+            </button>
+          </div>
+          <div className="col-span-2 grid grid grid-cols-2 gap-5 grid-rows-3">
             <Announcements />
             <LiveChat />
-            <ScoreGraphs/>
-        
-        
+            <ScoreGraphs />
+            <DiscussBoard />
+            <CourseMaterials />
+            <Assignment />
+          </div>
         </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-// this function will check for 
+// this function will check for
 // course details, instructor_name
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const courseId = Number(Array.isArray(context.params.id) ? context.params.id[0] : context.params.id)
-    const course = await prisma.course.findUnique({
-        where: { id: courseId },
-        include: { instructor: true },
-    })
+  const courseId = Number(
+    Array.isArray(context.params.id) ? context.params.id[0] : context.params.id
+  );
+  const course = await prisma.course.findUnique({
+    where: { id: courseId },
+    include: { instructor: true },
+  });
 
-    return { props: { course: { ...JSON.parse(JSON.stringify(course)), instructor_name: course.instructor.name } } }
-}
+  return {
+    props: {
+      course: {
+        ...JSON.parse(JSON.stringify(course)),
+        instructor_name: course.instructor.name,
+      },
+    },
+  };
+};
 
 export default CourseMenu;
